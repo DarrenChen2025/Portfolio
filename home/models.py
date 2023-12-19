59cd28c2c535ea1ccdc23f039613dda6341f1cba
DarrenChen2025 <Chen.darren@ufl.edu>
2023-12-19 18:37:30 -0500

    First Commit

---
+++ home/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import uuid
+from django.conf import settings
+
+# Create your models here.
+
+class Project(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True)
+    demo_link = models.URLField(max_length=200, null=True, blank=True)
+    source_link = models.URLField(max_length=200, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
+
+# Create your views here.
+
+def home(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+
+    return render(request, 'index.html', context)
