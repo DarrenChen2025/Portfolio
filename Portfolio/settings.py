b91bcf7d57db2e2926beb10aeac87c809404ec9c
DarrenChen2025 <Chen.darren@ufl.edu>
2023-12-26 00:46:14 -0500

    Migrate model changes

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # My Apps
     'home.apps.HomeConfig',
     'storages',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
